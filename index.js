955d06f1c84f41e4eb5a6bd7bbca6710a492874d
Kevin Damian Ta <kevindamianta@gmail.com>
2019-06-19 06:17:19 -0700

    Need to Access JSON
    Need to be able to access array in JSON so that we can iterate over the messages to get the Repo name, and URL displayed onto the DOM.

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 'use-strict';
 
-const searchURL = 'https://api.github.com'
-
-function getURL() {
+function getRepository() {
+	const userHandle = $('#js-text-field').val();
+	const url = `https://api.github.com/users/${userHandle}/repos`;
 	
+	fetch(url)
+		.then(response => {
+			if(response.ok) {
+				return response.json();
+			}
+			throw new Error(response.statusText);
+			})
+		.then(responseJson => console.log(responseJson))
+		.catch(error => alert('Something went wrong. Try again later.'));
 }
 
-function retreiveRepository() {
-	.fetch('https://api.github.com/users/kevinakadamian19/repos')
-	.then(response => response.json())
-	.then(responseJson => console.log(responseJson))
-	.catch(error => alert('Something went wrong. Try again later.'));
+function displayRespository(responseJson) {
+	console.log(responseJson);
+	$('.results').empty();
+	/*for(i = 0, i <= responseJson.length, i++) {
+	$('.results').append(`
+		<h2>${responseJson[i].name}</h2>
+		<h3>${responseJson[i].html_url}</h3>
+		`)};*/
+	$('.results').removeClass('hidden');
 }
 
 function watchSubmit() {
 	$('form').submit(event => {
 		event.preventDefault();
-		retreiveUrl();
+		getRepository();
+
 	});
 }
 
